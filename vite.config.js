0ade7f2749b2eef09b6faf4aae05a6b329a7817d
Celian Gremy <celian.gremy@gmail.com>
2026-05-09 23:06:21 +0200

    Set base path for GitHub deployment
    Added base path for GitHub repository.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // AJOUTE CETTE LIGNE : le nom de ton dépôt GitHub entre deux slashs
+  base: '/blindtest-nicou-theme/', 
   plugins: [react()],
 })
