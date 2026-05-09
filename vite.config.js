d9d7d74b4327332b3e9a8493f6955ee73d74a4cf
Munce-95 <celian.gremy@gmail.com>
2026-05-09 23:09:04 +0200

    Fix: ajustement base path et chemins relatifs

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // AJOUTE CETTE LIGNE : le nom de ton dépôt GitHub entre deux slashs
-  base: '/blindtest-nicou-theme/', 
+  base: '/blindtest-nicou-theme/', // <-- Très important : slash au début ET à la fin
   plugins: [react()],
 })
